253efcf4b4fec755189140e9296eedf481c19e31
миша <@MISHA>
2017-03-13 15:58:44 +0400

    Добавьте файлы проекта.

---
+++ shop/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace shop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text == "admin" && textBox1.Text == "1234")
+            {
+               
+                adminwindow a = new adminwindow();
+                a.Show();
+                Close();
+            }
+
+            if (textBox.Text == "guest")
+            {
+                guestwindow a = new guestwindow();
+                a.Show();
+                Close();
+            }
+        }
+    }
+}
---
+++ shop/clothes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shop
+{
+    class clothes
+    {
+
+        private string type;
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        private char size;
+
+        public char Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        private string color;
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        public clothes(string type, char size, string color, int count)
+        {
+            this.type = type;
+            this.size = size;
+            this.color = color;
+            this.count = count;
+        } 
+
+    }
+}
